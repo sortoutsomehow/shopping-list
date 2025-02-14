06fc83435f5c0bdc73f614d29c8d68be01a14685
sortoutsomehow <alighaffary4174@gmail.com>
2025-02-14 16:10:47 +0330

    initail commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const itemFrom  = document.getElementById('item-form')
+const itemInput  = document.getElementById('item-input')
+const itemList  = document.getElementById('item-list')
+
+
+function addItem(e){
+    e.preventDefault()
+    const newItem = itemInput.value
+
+    // validate input
+    if (newItem === ''){
+        alert('Please add an item')
+        return
+    }
+
+    // create list item
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(newItem))
+
+    const button = creaeteButton('remove-item btn-link text-red')
+    li.appendChild(button)
+    itemList.appendChild(li)
+
+    itemInput.value = ''
+}
+
+function creaeteButton(classes){
+    const button = document.createElement('button')
+    button.className = classes
+    const icon = createIcon('fa-solid fa-xmark')
+    button.appendChild(icon)
+    return button
+}
+
+function createIcon(classes){
+    const icon = document.createElement('i')
+    icon.className = classes
+    return icon
+}
+itemFrom.addEventListener('submit', addItem)
